0d1acab4fcd290dcbd179491927d3fcc7845ffbb
Derek Christensen <derek@heavyd.net>
2013-10-10 13:43:59 -0600

    Fixed master key handling problems.

--- SQRL.Client/Identity.cs
+++ SQRL.Client/Identity.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Security.Cryptography;
 using System.Text;
 using CryptSharp.Utility;
 using Sodium;
@@ -17,7 +16,7 @@ namespace SQRL.Client
 
         private readonly string _name;
         private readonly string _password;
-        private byte[] _derivedMasterKey;
+        private byte[] _encryptedMasterKey;
         private byte[] _masterKeySalt;
 
         private Identity(string name, string password)
@@ -58,42 +57,39 @@ namespace SQRL.Client
             byte[] generatedEntropy = Sodium.Random.GetBytes(RandomEntropyBytes);
             _masterKeySalt = Sodium.Random.GetBytes(RandomEntropyBytes);
 
-            var master = SCrypt.ComputeDerivedKey(generatedEntropy, entropy, ScryptIterations, 1024, 1, null, KeyWidth);
+            _encryptedMasterKey = SCrypt.ComputeDerivedKey(generatedEntropy, entropy, ScryptIterations, 1024, 1, null, KeyWidth);
 
-            GenerateDerivedMasterKey(master);
-
-            SaveIdentity(master);
-
-            Array.Clear(master, 0, master.Length);
+            SaveIdentity();
         }
 
-        private void GenerateDerivedMasterKey(byte[] master)
+        private byte[] CryptWithPassword(byte[] master)
         {
             var passwordHashBytes = GetPasswordHash();
-            _derivedMasterKey = new byte[KeyWidth];
+            var buffer = new byte[KeyWidth];
 
             for (int i = 0; i < master.Length; i++)
             {
-                _derivedMasterKey[i] = (byte) (master[i] ^ passwordHashBytes[i]);
+                buffer[i] = (byte)(master[i] ^ passwordHashBytes[i]);
             }
 
             Array.Clear(passwordHashBytes, 0, passwordHashBytes.Length);
+            return buffer;
         }
 
-        private void SaveIdentity(byte[] master)
+        private void SaveIdentity()
         {
             if (StorageProvider == null)
             {
                 return;
             }
 
-            var verfier = CryptoHash.SHA256(_derivedMasterKey);
-            Array.Resize(ref verfier, 16);
+            var verifier = GetVerifierHash();
+
             var store = new IdentityStore
                 {
-                    MasterKey = master,
+                    MasterKey = _encryptedMasterKey,
                     Salt = _masterKeySalt,
-                    Verifier = verfier,
+                    Verifier = verifier,
                     Iterations = ScryptIterations
                 };
 
@@ -101,9 +97,8 @@ namespace SQRL.Client
             var stream = new MemoryStream();
             serializer.Serialize(stream, store);
 
-            var protectedData = ProtectedData.Protect(stream.GetBuffer(), null, DataProtectionScope.CurrentUser);
-            string encryptedString = Convert.ToBase64String(protectedData);
-            StorageProvider.Save(_name, encryptedString);
+            string storeString = Convert.ToBase64String(stream.GetBuffer());
+            StorageProvider.Save(_name, storeString);
         }
 
         private void LoadIdentity()
@@ -114,14 +109,13 @@ namespace SQRL.Client
             }
 
             string encryptedString = StorageProvider.Load(_name);
-            var protectedData = Convert.FromBase64String(encryptedString);
-            var data = ProtectedData.Unprotect(protectedData, null, DataProtectionScope.CurrentUser);
+            var data = Convert.FromBase64String(encryptedString);
             
             var serializer = new BinaryFormatter();
             var store = (IdentityStore)serializer.Deserialize(new MemoryStream(data));
 
             _masterKeySalt = store.Salt;
-            GenerateDerivedMasterKey(store.MasterKey);
+            _encryptedMasterKey = store.MasterKey;
 
             VerifyIdentity(store.Verifier);
         }
@@ -137,9 +131,11 @@ namespace SQRL.Client
 
         private byte[] GetVerifierHash()
         {
-            var verfier = CryptoHash.SHA256(_derivedMasterKey);
-            Array.Resize(ref verfier, 16);
-            return verfier;
+            var master = CryptWithPassword(_encryptedMasterKey);
+            var verifier = CryptoHash.SHA256(master);
+            Array.Clear(master, 0, master.Length);
+            Array.Resize(ref verifier, 16);
+            return verifier;
         }
 
         private byte[] GetPasswordHash()
@@ -150,7 +146,10 @@ namespace SQRL.Client
 
         public byte[] GetSitePrivateKey(string domain)
         {
-            return CryptoAuth.AuthHmacSha512_256(domain, _derivedMasterKey);
+            var master = CryptWithPassword(_encryptedMasterKey);
+            byte[] siteKey = CryptoAuth.AuthHmacSha512_256(domain, master);
+            Array.Clear(master, 0, master.Length);
+            return siteKey;
         }
 
         [Serializable]
